08e97694746adaf21a482226137e18590d33e62a
Profproject <ihorkret13@gmail.com>
2026-01-19 22:36:32 +0100

    Update models.py

--- models.py
+++ models.py
@@ -4,50 +4,28 @@ import datetime
 
 Base = declarative_base()
 
-
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
-
-    # рефералка
-    referrer_id = Column(Integer, nullable=True)
-
-    # балансы
     balance = Column(Float, default=0)
-    
-
-    # флаги
-    visit_reward_given = Column(Boolean, default=False)
     activated = Column(Boolean, default=False)
-
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-
 
 class WithdrawRequest(Base):
     __tablename__ = "withdraw_requests"
-
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer)
-
     address = Column(String)
     memo = Column(String)
-
     amount = Column(Float, default=0)
     status = Column(String, default="pending")
-
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-
 
 class AdRequest(Base):
     __tablename__ = "ad_requests"
-
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer)
-
     package = Column(Integer)  # 25 / 75 / 250
     link = Column(String)
-
     status = Column(String, default="pending")
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-
